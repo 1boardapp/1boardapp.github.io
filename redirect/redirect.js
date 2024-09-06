c2ac99dd6e8e7694ff6f32a4c042760b4dd4491d
timonharz <timon.harz@icloud.com>
2024-09-06 22:48:49 +0200

    Changes

--- redirect/redirect.js
+++ redirect/redirect.js
@@ -1,5 +1,16 @@
-function redirect(url) {
-    window.location.href = url;
+function redirect() {
+    var appUrl = "Oneboard://";
+    var fallbackUrl = "https://fallback-website.com"; // Replace with your fallback URL
+
+    // Attempt to open the app
+    window.location.href = appUrl;
+
+    // Set a timeout to redirect to the fallback URL if the app doesn't open
+    setTimeout(function() {
+        if (!document.hidden) {
+            window.location.href = fallbackUrl;
+        }
+    }, 2000); // Wait for 2 seconds before redirecting
 }
 
 redirect("Oneboard://");
